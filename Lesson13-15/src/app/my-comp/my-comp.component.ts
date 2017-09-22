5b27083d23280f60d74e34a2085ac4ff2b946eed
Amh <abaams78@gmail.com>
2017-09-21 21:40:35 -0500

    Lesson15 lab second commit

--- Lesson13-15/src/app/my-comp/my-comp.component.ts
+++ Lesson13-15/src/app/my-comp/my-comp.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, FormControl, Validators, FormBuilder, FormArray, FormGroup } from '@angular/forms';
+
+import { DataService } from '../service/data.service'
 
 @Component({
   selector: 'app-my-comp',
@@ -6,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-comp.component.css']
 })
 export class MyCompComponent implements OnInit {
+  myForm: FormGroup;
+  private userData;
+  private userPost;
+  constructor(private fb: FormBuilder, private dataService: DataService) {
+    this.myForm = this.fb.group({
+      'name' : ['', Validators.required],
+      'email': ['', Validators.compose([Validators.required, Validators.email])],
+      'post' : ['', Validators.compose([Validators.required, this.legthValidator])]
+    });
+    this.myForm.statusChanges.subscribe(data => console.log(data));
+   }
 
-  constructor() { }
+   legthValidator(control: FormControl):{[str: string]: boolean}{
+      if (control.value.length < 10) {
+        return { 'invalid': true };
+      }
+      return null;
+   }
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+    console.log(this.myForm);
+  }
+
+  getData(){
+    this.userData = new Promise((resolve, reject)=>{
+      this.dataService.getUser().subscribe(res => resolve(res.json()));
+    });
+
+    this.userPost = new Promise((resolve, reject)=>{
+      this.dataService.getPost().subscribe(res => resolve(res.json()));
+    });
   }
 
 }
